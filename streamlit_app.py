2025d421a415d98dd65bd5e62ea334023961ff26
zhiming-dai <108203956+zhiming-dev@users.noreply.github.com>
2023-10-04 22:59:02 -0400

    Merged model and auth

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if not cookies.ready():
 LOGGED_IN_USER_NAME = cookies.get('logged_in_users_name')
 
 
-def load_model():
+def load_model(a,b,w,l0,l1,p):
     global model
     model = tf.keras.models.load_model('models/NN_4_64_64_1/converted_model.h5')
     a = np.array([[1, 1, 1, 1]])
@@ -53,7 +53,8 @@ def load_model():
     st.write('pred:', pred)
 
 
-def KP(a, b, w, l0, l1, P):
+
+def KP(a, b, w, l0, l1):
     inputs = np.array([[a / b, w / b, l0 / b, l1 / b]])
     outputs = model.predict(inputs)
     kop = (outputs * l1 * (a ** 0.5) / b / w / w)
@@ -66,6 +67,44 @@ def emp(a, b, w, L, P):
     I = w * b ** 3 / 12 + (y - b / 2) ** 2 * b * w + w ** 4 / 288 + (w / 6 + b - y) ** 2 * w * w / 4
     sigma = P * L * y / I
     return sigma * np.sqrt(np.pi * a) * F
+
+
+def display_values(a, b, w, l0, l1, P, K_o, record_num):
+    print(type(a), type(b), type(w), type(l0), type(l1), type(P), type(K_o))
+    try:
+        K_o = float(K_o)
+        str_output = ("a={:.3f}, b={:.3f}, w={:.3f}, L0={:.3f}, L1={:.3f}, P={:.3f}, KI={:.3f}".format(a, b, w, l0, l1, P, K_o))
+        
+        
+        st.write(str_output)
+        st.write('Empirical:  {:.3f}'.format(emp2(a, b, w, l1, P)))
+        
+        if (0.1 <= a/b <= 0.8 and 1.0 <= w/b <= 3.0 and
+            0.1 <= l0/b <= 0.4 and 2.0 <= l1/b <= 5.0):
+            st.markdown(':smile: Interpolation')
+            st.write("Record {}:  {} (Interpolation)".format(pad(record_num, 3), str_output))
+        else:
+            st.markdown(':frowning_face: Extrapolation')
+            st.write("Record {}:  {} (Extrapolation)".format(pad(record_num, 3), str_output))
+        
+        st.write("KI Value: {:.3f}".format(K_o))
+    except:
+        st.write("invalid input!")
+
+
+
+
+def emp2(a, b, w, L, P):
+    F = 1.85 - 3.38*(a/b) + 13.24*pow(a/b, 2) - 23.26*pow(a/b, 3) + 16.8*pow(a/b, 4)
+    y = (pow(b, 2) * w / 2 + pow(w, 2) / 4 * (b + w / 6)) / (b * w + pow(w, 2) / 4)
+    I = (w*pow(b, 3)/12 + pow(y-b/2, 2)*b*w + pow(w, 4)/288 + pow(w/6+b-y, 2)*pow(w, 2)/4)
+    sigma = P * L * y / I
+    return sigma * (pow(a, 0.5) * 3.14159) * F
+
+
+def pad(n, width, z='0'):
+    return (str(z) * (width - len(str(n))) + str(n))
+
 
 def runUiSetUp():
     st.image("./public/web_title.png")
@@ -303,8 +342,6 @@ with st.sidebar:
 
         # TODO: Check parsing json and apply model equation
 
-# load demo model here
-load_model()
 
 sessionData = getLoggedInUserFromCookie(cookies)
 print(sessionData)
@@ -316,8 +353,6 @@ else:
 
 renderAuthModals(sessionData['success'], cookies)
 
-st.write("(PoC) Assuming model equation is: a + b + w + LZero + LOne + P. Result is: " )
-st.write(str(a + b + w + LZero + LOne + P))
 
 col1, col2 = st.columns(2, gap="medium")
 with col1:
@@ -326,6 +361,19 @@ with col1:
 with col2:
    with st.container():
     st.components.v1.iframe(iframe_src_3d_url, scrolling=False)
+
+# load demo model here
+# load_model()
+
+model = tf.keras.models.load_model('models/NN_4_64_64_1/converted_model.h5')
+if b == 0:
+   st.write('Please input parameters!')
+else:
+    kop = KP(a,b,w,LZero,LOne)
+    ki = kop * P
+    st.write('KI:', ki)
+
+    display_values(a, b, w, LZero, LOne, P, ki, record_num)
 
 st.write("*****DEBUG: Current cookies:", cookies)
 
